9e0c79cf46749aef4e618113edd1eef6bdc59bc9
Aaron Lewkowiez <al06208@georgiasouthern.edu>
2019-04-26 15:17:50 -0400

    fdsafd

--- SQLTestingWithGUI/src/testGUIThree/PassengerMaker.java
+++ SQLTestingWithGUI/src/testGUIThree/PassengerMaker.java
@@ -51,7 +51,7 @@ public class PassengerMaker extends JFrame {
 	 */
 	public PassengerMaker() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 190, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
